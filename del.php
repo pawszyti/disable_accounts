a25a3480234f157292fcf39d4c6b1da2e8e0fa89
Paweł Szymczyk <pawszyti@gmail.com>
2017-01-22 14:17:37 +0100

    bata_alerty

--- del.php
+++ del.php
@@ -2,7 +2,28 @@
 session_start();
 require('config/config.php');
 $id = $_GET['id'];
+
+$zapytanie2 = "SELECT * FROM rozliczenia WHERE r_id=$id";
+$wynik2 = $db13->query($zapytanie2);
+$ilosc2 = $wynik2->num_rows;
+if ($ilosc2<1)
+{
+    $_SESSION['alert'] = " <div class=\"alert\">
+    <span class=\"closebtn\" onclick=\"this.parentElement.style.display='none';\">&times;</span>
+    Nie ma takiego użytkownika 
+    </div>";
+    header('location: main.php');
+    exit();
+}
+else
+{
+$tablica2 = $wynik2->fetch_assoc();
+$_SESSION['alert'] = " <div class=\"alert2\">
+<span class=\"closebtn\" onclick=\"this.parentElement.style.display='none';\">&times;</span>
+Usunięto poprawnie z listy użytkownika ".$tablica2['r_imie']." ".$tablica2['r_nazwisko']." 
+</div>";
 $zapytanie = "DELETE FROM rozliczenia WHERE r_id=$id";
 $db13->query($zapytanie);
 header('location: main.php');
+}
 ?>
