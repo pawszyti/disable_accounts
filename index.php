e29ee201c6171ab20ed4ed3657e99c5c2b9d9e79
Paweł Szymczyk <pawszyti@gmail.com>
2017-01-26 11:29:09 +0100

    okno potwierdzenia usunięcia

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 header("location: main.php");
 ?>
+
--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <?php
-    session_start();
-    require_once ('config/config.php');
+session_start();
+require_once ('config/config.php');
+
 ?>
 
 <!DOCTYPE html>
@@ -10,20 +11,35 @@
     <meta name="author" content="Paweł Szymczyk" />
     <title>Rozliczenia użytkowników</title>
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <script src="js/jquery-3.1.1.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootbox.min.js"></script>
+
 </head>
 <body>
 
 <div class="menu_first">
     <div class="menu_second">
+
+
+
         <a href="dodaj.php" class="myButton">Dodaj</a>
+
+
         <span class="tytul">Rozliczenia</span>
+
         <a href="cron_rozliczenia.php" class="myButton">CRON</a>
+
     </div>
 </div>
 
 <?php
-    echo $_SESSION['alert'];
-    session_unset($_SESSION['alert']);
+echo $_SESSION['alert'];
+session_unset($_SESSION['alert']);
+
+
 ?>
 <table class="tabela2" cellspacing='0'>
     <tr>
@@ -39,43 +55,54 @@
     </tr>
 
     <?php
-        $zapytanie = "SELECT * FROM rozliczenia ORDER BY r_id DESC;";
-        $wynik = $db13->query($zapytanie);
-        $ilosc = $wynik->num_rows;
-        $licznik = $ilosc;
-        for ($i = 0; $i < $ilosc; $i++) {
-            $tablica = $wynik->fetch_assoc();
-            echo "
+    $zapytanie = "SELECT * FROM rozliczenia ORDER BY r_id DESC;";
+    $wynik = $db13->query($zapytanie);
+    $ilosc = $wynik->num_rows;
+    $licznik = $ilosc;
+    for ($i = 0; $i < $ilosc; $i++)
+    {
+        $tablica = $wynik->fetch_assoc();
+        echo "
             <tr>
-            <td>".$licznik."</td>
-            <td>".$tablica['r_imie']."</td>
-            <td>".$tablica['r_nazwisko']."</td>
-            <td>".$tablica['r_pesel']."</td>
-            <td>".$tablica['r_data']."</td>
-            <td><img src=\"img/".$tablica['r_status'].".png\" width=\"25px\" height=\"25px\"></td>";
+        <td>".$licznik."</td>
+        <td>".$tablica['r_imie']."</td>
+        <td>".$tablica['r_nazwisko']."</td>
+        <td>".$tablica['r_pesel']."</td>
+        <td>".$tablica['r_data']."</td>
+        <td><img src=\"img/".$tablica['r_status'].".png\" width=\"25px\" height=\"25px\"></td>";
 
-            $pesel = $tablica['r_pesel'];
-            $zapytanie_status = "SELECT * FROM pracownicy_ewidencja WHERE pesel LIKE $pesel";
-            $wynik_status = $db2->query($zapytanie_status);
-            $tablica_status = $wynik_status->fetch_assoc();
-            if ($tablica_status['czy_pracuje'] == 1) {
-                $ikona = "no";
-            }
-            else {
-                $ikona = "ok";
-            }
-            echo"
-            <td><img src=\"img/".$ikona.".png\" width=\"25px\" height=\"25px\"></td>
-            <td><img src=\"img/".$tablica['r_samba'].".png\" width = \"25px\" height = \"25px\" ></td >";
-            $licznik = $licznik-1;
-            if ($tablica['r_status']==no)    {
-                echo "<td><a href=\"del.php?id=".$tablica['r_id']."\" class=\"myButton2\">Usuń</a></td>";
-            }
-            else {
-                echo "<td> </td>";
-            }
-            echo" </tr></table>";
+        $pesel = $tablica['r_pesel'];
+        $zapytanie_status = "SELECT * FROM pracownicy_ewidencja WHERE pesel LIKE $pesel";
+        $wynik_status = $db2->query($zapytanie_status);
+        $tablica_status = $wynik_status->fetch_assoc();
+        if ($tablica_status['czy_pracuje'] == 1) {
+            $ikona = "no";
+        } else {
+            $ikona = "ok";
         }
+        echo"
+        <td><img src=\"img/".$ikona.".png\" width=\"25px\" height=\"25px\"></td>
+        <td><img src=\"img/".$tablica['r_samba'].".png\" width = \"25px\" height = \"25px\" ></td >
+        
+        ";
+        $licznik = $licznik-1;
+        if ($tablica['r_status']==no)    {
+            echo "<td><input type=submit class=\"btn\" value='usuń' onclick=\"bootbox.confirm('Czy chcesz z listy usunąć użytkownika<b> ".$tablica['r_imie']." ".$tablica['r_nazwisko']."</b> ?', function(result){ if (result==true) {window.location.href='del.php?id=".$tablica['r_id']."'}; });\" class=\"myButton2\"></td>";
+        }
+        else
+        {
+            echo "<td> </td>";
+        }
+        echo" </tr>";
+    }
+    echo "<script type='text/javascript'>
+	</script>\n";
     ?>
+
+
+
+
+</table>
+
 </body>
 </html>
